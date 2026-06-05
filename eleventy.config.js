76859b5396d6f5a86f5c1234fc0e28fd0059fb4d
John Gravois <jgravois@compiler.la>
2026-06-05 17:55:16 +0000

    chore: rewire RSS feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 const byTitle = (a, b) => a.data.title.localeCompare(b.data.title);
 
 export default async function (eleventyConfig) {
@@ -8,17 +10,24 @@ export default async function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/styles/");
 
   // Keeps the same directory structure.
-  eleventyConfig.addPassthroughCopy("src/_redirects");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("**/*.jpg");
-  eleventyConfig.addPassthroughCopy("**/*.jpeg");
-  eleventyConfig.addPassthroughCopy("**/*.png");
-  eleventyConfig.addPassthroughCopy("**/*.svg");
+  eleventyConfig
+    .addPassthroughCopy("src/_redirects")
+    .addPassthroughCopy("src/favicon.ico")
+    .addPassthroughCopy("**/*.jpg")
+    .addPassthroughCopy("**/*.jpeg")
+    .addPassthroughCopy("**/*.png")
+    .addPassthroughCopy("**/*.svg")
+    .addPassthroughCopy("src/blog/pretty-atom-feed.xsl"); //
 
   eleventyConfig.addPassthroughCopy("src/assets/");
 
-  eleventyConfig.addCollection("jobs", (collectionApi) => {
-    return collectionApi.getFilteredByGlob("src/jobs/*.md").sort(byTitle);
+  eleventyConfig.addCollection("jobs", (collectionsApi) => {
+    return collectionsApi.getFilteredByGlob("src/jobs/*.md").sort(byTitle);
+  });
+
+  // ensure draft blog posts are omitted from the RSS feed
+  eleventyConfig.addCollection("publishedPosts", (collectionsApi) => {
+    return collectionsApi.getFilteredByTag("posts").filter((post) => !post.data.draft);
   });
 
   // draft blog posts are rendered locally, but not in production builds
@@ -30,5 +39,20 @@ export default async function (eleventyConfig) {
     if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
       return false;
     }
+  });
+
+  // RSS feed - example https://github.com/11ty/eleventy-base-blog/
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "/blog/feed.xml",
+    stylesheet: "pretty-atom-feed.xsl",
+    collection: { limit: 20, name: "publishedPosts" },
+    metadata: {
+      language: "en",
+      title: "Compiler’s Blog",
+      subtitle:
+        "Software built by humans, for humans, in LA. Compiler is a woman-owned software consultancy that’s passionate about making government tech solutions easy-to-use and accessible for all.",
+      base: "https://compiler.la/blog/",
+    },
   });
 }
--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -1,4 +1,8 @@
+// /blog/:year/:slug/
+const permalink = ({ page: { date, fileSlug } }) => `/blog/${date.getFullYear()}/${fileSlug}/`;
+
 export default {
   layout: "blog_post",
+  permalink,
   tags: ["posts"],
 };
